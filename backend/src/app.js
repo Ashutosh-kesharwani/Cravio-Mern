a6c8d8026e9a64cd2b76474a17f95e569043a65c
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-07-08 22:56:17 +0530

    feat: Add global middleware configuration for CORS, JSON parsing, URL encoding, static files, and cookie parsing

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,40 @@
+import cookieParser from "cookie-parser";
+import cors from "cors";
 import express from "express";
 const app = express();
+
+// Global middleware
+
+// 1) Cors middleware
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// 2) Req Data Configuration
+// Use to set in Advance
+// req data -> must be sent always in json form , with limit of 16kb
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+// 3) Url encoded data Configuration
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
+
+// 4) Middleware For setting static folder
+// Public -> temp local storage
+app.use(express.static("public"));
+
+// 5) Cookie-Parser : For reading cookie from req which sent by browser
+app.use(cookieParser());
 
 export { app };
