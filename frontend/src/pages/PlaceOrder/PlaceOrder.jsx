00d06f2a4dae0d4ce92c171cec66da154de7f470
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-07-14 19:14:07 +0530

    feat: enhance PlaceOrder component with form handling and order submission; implement input validation and redirect on successful order placement

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,27 +1,165 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import api from "../../api/axios.js";
 import { useStore } from "../../context/storeContext";
 import "./PlaceOrder.css";
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useStore();
+  const { getTotalCartAmount, food_list, cartItems, user } = useStore();
+  // token to cookie me hai
+  //  url kha se lo ?
+  const navigate = useNavigate();
+  // Create a form state variable
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    contactNumber: "",
+  });
+
+  // Create a handleChange func
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setData((prev) => ({ ...prev, [name]: value }));
+  };
+  // When we submit the form , this func will get executed
+  const placeOrder = async (event) => {
+    event.preventDefault();
+
+    // Before passing the data in api, we have to get all the items that user has uploaded in cart
+    let orderItems = [];
+    food_list.map((item) => {
+      // check if our cartItems have product with this id , then add its food info into orderItems
+      if (cartItems[item._id] > 0) {
+        let itemInfo = item;
+        itemInfo["quantity"] = cartItems[item._id];
+        orderItems.push(itemInfo);
+      }
+    });
+
+    // Create a orderData which we are going to send to our server in post req
+    let orderData = {
+      address: data,
+      items: orderItems,
+    };
+    try {
+      // orders/place
+      const response = await api.post("/orders/place", orderData);
+      if (response.data.success) {
+        // If req was successfull , then we get session_url from server
+        const { session_url } = response.data.data;
+
+        // send user to this session url
+        window.location.replace(session_url);
+        toast(response.data.message);
+      }
+    } catch (error) {
+      const message =
+        error.response?.data?.message ||
+        error.message ||
+        "Something went wrong";
+      toast.error(message);
+    }
+  };
+
+  // To prevent user to go to orders page
+  // If user is not authenticated
+  // If cart is empty
+  useEffect(() => {
+    if (!user) {
+      navigate("/cart");
+    } else if (getTotalCartAmount() === 0) {
+      navigate("/cart");
+    }
+  }, [user]);
 
   return (
-    <form className="place-order">
+    <form className="place-order" onSubmit={placeOrder}>
       <div className="place-order-left">
         <p className="title">Delivery Information</p>
         <div className="multi-fields">
-          <input type="text" placeholder="First name" />
-          <input type="text" placeholder="Last name" />
+          <input
+            type="text"
+            required
+            placeholder="First name"
+            name="firstName"
+            value={data.firstName}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            required
+            placeholder="Last name"
+            name="lastName"
+            value={data.lastName}
+            onChange={handleChange}
+          />
         </div>
-        <input type="text" placeholder="Email address" />
-        <input type="text" placeholder="Street" />
+        <input
+          type="text"
+          required
+          placeholder="Email address"
+          name="email"
+          value={data.email}
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          required
+          placeholder="Street"
+          name="street"
+          value={data.street}
+          onChange={handleChange}
+        />
         <div className="multi-fields">
-          <input type="text" placeholder="City" />
-          <input type="text" placeholder="State" />
+          <input
+            required
+            type="text"
+            placeholder="City"
+            name="city"
+            value={data.city}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            required
+            placeholder="State"
+            name="state"
+            value={data.state}
+            onChange={handleChange}
+          />
         </div>
         <div className="multi-fields">
-          <input type="text" placeholder="Zip code" />
-          <input type="text" placeholder="Country" />
+          <input
+            type="text"
+            required
+            placeholder="Zip code"
+            name="zipcode"
+            value={data.zipcode}
+            onChange={handleChange}
+          />
+          <input
+            type="text"
+            required
+            placeholder="Country"
+            name="country"
+            value={data.country}
+            onChange={handleChange}
+          />
         </div>
-        <input type="text" placeholder="Contact number" />
+        <input
+          type="text"
+          required
+          placeholder="Contact number"
+          name="contactNumber"
+          value={data.contactNumber}
+          onChange={handleChange}
+        />
       </div>
       <div className="place-order-right">
         {/* Here cart total automatically inherit the style as we already define it in cart.css so as at time of render component everything got render so as css . so in placeorder.css we dont have to put style */}
@@ -54,7 +192,7 @@ const PlaceOrder = () => {
           </div>
 
           {/* On click proceed to checkout navigate to place-order page */}
-          <button>PROCEED TO PAYMENT</button>
+          <button type="submit">PROCEED TO PAYMENT</button>
         </div>
       </div>
     </form>
