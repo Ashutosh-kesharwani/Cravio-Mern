ecfb05ae5ffb272f58cc9460f7b053b9eb0c3ba9
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-07-15 18:05:30 +0530

    feat: implement global error handling and graceful shutdown in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,49 @@
+import { app } from "./app.js";
 import connectDB from "./db/server.js";
-// Test loading or env variable
-console.log(process.env.PORT);
+// PORT
+const PORT = process.env.PORT;
 
-connectDB();
+// Process Level Error Handling [Global]
+
+// Synchronous Errors
+process.on("uncaughtException", (error) => {
+  console.error("Uncaught Exception :: ", error);
+  process.exit(1);
+});
+
+// Unhandled Promise Rejections [Asynchronous Error]
+process.on("unhandledRejection", (error) => {
+  console.log(`Unhandled Rejection :: ${error}`);
+  process.exit(1);
+});
+
+// Graceful Shutdown
+process.on("SIGINT", () => {
+  console.log("\nServer Shutting Down Gracefully");
+  process.exit(0);
+});
+
+// DB Connection & Server Start
+connectDB()
+  .then(() => {
+    // Express App error
+    app.on("error", (error) => {
+      console.log(`Express :: ERROR :: ${error}`);
+      process.exit(1);
+    });
+
+    // Start Server
+    const server = app.listen(PORT, () => {
+      console.log(`Server listening at  :: http://localhost:${PORT}`);
+    });
+
+    //  Server error
+    server.on("error", (error) => {
+      console.log(`SERVER :: ERROR :: ${error}`);
+      process.exit(1);
+    });
+  })
+  .catch((error) => {
+    console.log(`DB CONNECTION ERROR :: ${error} `);
+    process.exit(1);
+  });
