3e6ff13fc63660d6cccbc397735aee769e96f85b
Ashutosh <ashutoshkesharwani20021406@gmail.com>
2026-07-15 17:42:32 +0530

    feat: add CORS middleware and update .env.sample for allowed origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,38 @@
+import cors from "cors";
 import express from "express";
-
 const app = express();
+
+// GLOBAL MIDDLEWARE
+
+// 1. CORS
+const allowedOrigins = process.env.CORS_ORIGINS;
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      // Allow requests with no origin (Postman, mobile apps, etc.) || if origin then must be in allowed origins
+      if (!origin || allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
+
+//2. Json Configuration
+// req -> body -> data must be always send in json
+app.use(
+  express.json({
+    limit: "16kb",
+  })
+);
+
+//3. URL encoded configuration
+app.use(
+  express.urlencoded({
+    extended: true,
+    limit: "16kb",
+  })
+);
 
 export { app };
